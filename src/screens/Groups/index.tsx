9412b3860b5a7678bfecce61aa43c67f535ed454
Lucas Castilho <lucas.hco@hotmail.com>
2023-04-20 07:58:03 -0300

    chore: project setup

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styles';
+
+export function Groups() {
+  return (
+    <S.Container>
+      <S.Title>Groups</S.Title>
+    </S.Container>
+  )
+}
