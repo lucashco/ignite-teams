5b9034b5bf70eb089131664103b0208036f506a7
Lucas Castilho <lucas.hco@hotmail.com>
2023-04-22 09:24:56 -0300

    feat: setup fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { ThemeProvider } from 'styled-components/native';
-import {Groups} from '@screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator, View } from 'react-native';
+import {useState} from 'react'
 import theme from '@theme/index';
 
+import {Groups} from '@screens/Groups';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
-    </ThemeProvider>
+      {fontsLoaded ? 
+        <Groups /> :  
+        <ActivityIndicator color="red"/>
+      }
+     </ThemeProvider>
   );
 }
 
