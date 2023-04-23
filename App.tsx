4f6841794245173a0c1015b7f0f31cfde356508a
Lucas Castilho <lucas.hco@hotmail.com>
2023-04-23 16:22:14 -0300

    feat: creates the app routes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from '@theme/index';
 
-import {Groups} from '@screens/Groups';
+import { Routes } from '@routes/index';
+
 import { Loading } from '@components/Loading';
-import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
         translucent
       />
       {fontsLoaded ?
-        <Groups /> :
+        <Routes /> :
         <Loading/>
       }
      </ThemeProvider>
