171bba834359fad60e4df18bd61abe0af87369b2
Lucas Castilho <lucas.hco@hotmail.com>
2023-05-01 15:52:21 -0300

    feat: creates custom error and throw it

--- src/storage/group/create.ts
+++ src/storage/group/create.ts
@@ -1,11 +1,18 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { AppError } from '@utils/AppError';
+
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 import { getGroups } from './list';
 
 export async function createGroup(groupName: string): Promise<void> {
   try {
     const storedGroups = await getGroups();
+    const groupAlreadyExists = storedGroups.includes(groupName);
+
+    if (groupAlreadyExists) {
+      throw new AppError('Esse grupo já existe.');
+    }
 
     storedGroups.push(groupName);
 
