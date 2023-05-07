a56f6187571a21f90824268ed4b406cb74d4cac2
Lucas Castilho <lucas.hco@hotmail.com>
2023-05-07 13:14:14 -0300

    feat: loads groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList, ListRenderItem } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { Header } from '@components/Header';
-import {Highlight} from '@components/Highlight';
-import {GroupCard} from '@components/GroupCard';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 import { Button } from '@components/Button';
 import { ListEmpty } from '@components/ListEmpty';
+
+import { getGroups } from '@storage/group/list';
 
 import * as S from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState([]);
+  const [groups, setGroups] = useState<string[]>([]);
   const navigation = useNavigation();
 
   const _renderItem: ListRenderItem<string> = ({ item }) => {
-    return <GroupCard title={item} />
+    return <GroupCard onPress={() => handleOpenGroup(item)} title={item} />
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
   }
 
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const storedGroups = await getGroups();
+      setGroups(storedGroups);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups();
+  }, []))
+
 
   return (
     <S.Container>
@@ -32,7 +52,7 @@ export function Groups() {
       />
 
       <FlatList
-        contentContainerStyle={{ flex: 1 }}
+        // contentContainerStyle={{ flex: 1 }}
         data={groups}
         renderItem={_renderItem}
         keyExtractor={item => item}
